d2e18dd4c366cbd884fe12770c0efa412493b8a2
Sarmad Sohail <42043799+Decabra@users.noreply.github.com>
2020-03-15 17:23:09 +0500

    FetchData Updated

--- app/src/main/java/com/example/ghc/FetchData.java
+++ app/src/main/java/com/example/ghc/FetchData.java
@@ -1,9 +1,30 @@
 package com.example.ghc;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.Build;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Spinner;
+
+import androidx.annotation.RequiresApi;
+
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class FetchData {
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class FetchData{
 
     private JSONObject jsonData;
     private JSONObject options;
@@ -14,9 +35,7 @@ public class FetchData {
     private JSONObject send_medical_list_after_minutes;
     private JSONArray check_after_time;
 
-    public void getRequestMethod(){
 
-    }
 
 
     public JSONObject getJsonData() {
@@ -27,7 +46,18 @@ public class FetchData {
         this.jsonData = jsonData;
     }
 
-    public JSONObject getOptions() {
+    JSONObject getOptions(String getOptionURL, OkHttpClient client) throws IOException, JSONException {
+        JSONObject options = null;
+        Request request = new Request.Builder().url(getOptionURL).get().build();
+        Response response = client.newCall(request).execute();
+        String reponseBody = response.body().string();
+        try {
+            JSONObject json = new JSONObject(reponseBody);
+            options = json.getJSONObject("options");
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
         return options;
     }
 
@@ -83,10 +113,35 @@ public class FetchData {
         this.check_after_time = check_after_time;
     }
 
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) activity.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(
+                activity.getCurrentFocus().getWindowToken(), 0);
+    }
 
+    void setSpinnerHeight(Spinner spinner, int height){
+        try {
+            Field popup = Spinner.class.getDeclaredField("mPopup");
+            popup.setAccessible(true);
 
+            // Get private mPopup member variable and try cast to ListPopupWindow
+            android.widget.ListPopupWindow popupWindow = (android.widget.ListPopupWindow) popup.get(spinner);
 
+            // Set popupWindow height to 500px
+            popupWindow.setHeight(height);
+        }
+        catch (NoClassDefFoundError | ClassCastException | NoSuchFieldException | IllegalAccessException e) {
+            // silently fail...
+        }
+    }
 
-
-
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    void progressLoader(ProgressDialog ProgressLoader){
+        ProgressLoader.show();
+        ProgressLoader.setContentView(R.layout.progress_dialog);
+        Objects.requireNonNull(ProgressLoader.getWindow()).setBackgroundDrawableResource(R.color.transparent);
+        ProgressLoader.setCanceledOnTouchOutside(false);
+    }
 }
